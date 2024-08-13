efd1e24f42c47550afe94c86425c55016125292b
Zahar2285 <Zahar2285@mail.ru>
2024-08-13 16:09:16 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name='Александр'
 print("Name:",name)
 age=36
 print("Age:",age)
-age=38
-print("New age:",age)
-is_student= 36!=38
+age_1=age+2
+print("New age:",age_1)
+is_student= age!=age_1
 print("is_student:",is_student)
